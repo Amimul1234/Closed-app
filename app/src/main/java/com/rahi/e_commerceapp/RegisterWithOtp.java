da453e6088e79466c307ec4fa510d5889c3c16f1
18050 <1805056@ugrad.cse.buet.ac.bd>
2020-07-04 00:15:57 +0600

    Phone number validation added successfully.

--- app/src/main/java/com/rahi/e_commerceapp/RegisterWithOtp.java
+++ app/src/main/java/com/rahi/e_commerceapp/RegisterWithOtp.java
@@ -4,8 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class RegisterWithOtp extends AppCompatActivity {
 
@@ -23,7 +36,7 @@ public class RegisterWithOtp extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String number = editText.getText().toString().trim();
+                final String number = editText.getText().toString().trim();
 
                 if (number.isEmpty() || number.length() < 11) {
                     editText.setError("Please enter a valid number");
@@ -31,14 +44,39 @@ public class RegisterWithOtp extends AppCompatActivity {
                     return;
                 }
 
-                String phoneNumber = "+" + "88" + number;
+                //For checking existing user in the database
 
-                Intent intent = new Intent(RegisterWithOtp.this, VerifyPhoneActivity.class);
-                intent.putExtra("phonenumber", phoneNumber);
-                startActivity(intent);
+                final DatabaseReference RootRef;
+                RootRef = FirebaseDatabase.getInstance().getReference();
 
+                RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        if (!(dataSnapshot.child("Users").child(number).exists()))//For checking existing users.......
+                        {
+                            String phoneNumber = "+" + "88" + number;
+
+                            Intent intent = new Intent(RegisterWithOtp.this, VerifyPhoneActivity.class);
+                            intent.putExtra("phonenumber", phoneNumber);
+                            startActivity(intent);
+                        }
+
+                        else {
+
+                            Toast.makeText(getApplicationContext(), "Phone number already exists", Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
             }
-        });
+         });
     }
-
 }
